0124174dd1151feee4a180a66582c32ca132bc92
Andrew <shatokhin.a.p@gmail.com>
2023-02-27 18:36:02 +0200

    rev 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -57,7 +57,7 @@ export default class App extends Component {
   };
 
   componentDidMount() {
-    console.log('DidMount');
+    // console.log('DidMount');
     const contacts = localStorage.getItem('contacts');
     // console.log(contacts);
     const parsedContacts = JSON.parse(contacts);
