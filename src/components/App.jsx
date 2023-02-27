27a7a2ddff19f1c6ef6d8ca40d03d127e009ee9a
Andrew <shatokhin.a.p@gmail.com>
2023-02-27 18:35:12 +0200

    rev 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,10 +9,10 @@ import { Title, TitleSection } from './App.styled';
 export default class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -55,6 +55,26 @@ export default class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
+  componentDidMount() {
+    console.log('DidMount');
+    const contacts = localStorage.getItem('contacts');
+    // console.log(contacts);
+    const parsedContacts = JSON.parse(contacts);
+    // console.log(parsedContacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log('componentDidUpdate');
+    if (this.state.contacts !== prevState.contacts) {
+      // console.log('Обновилось поле');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const filteredContacts = this.getFilteredContacts();
